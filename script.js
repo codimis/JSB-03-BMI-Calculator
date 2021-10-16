6027efd45561c931e36223b16588e95ac2985ba1
endergulger <endergulger@gmail.com>
2021-10-17 00:06:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ function calculatorBMI(){
 
 const height = Number(document.getElementById("height-input").value);
 const weight = Number(document.getElementById("weight-input").value);
-const result = Number(document.getElementById("result-text").value);
+const convert = height / 100;
 
-const bmi = (weight / (height * height)*10000).toFixed(2);
-
-document.getElementById("calculate-button").addEventListener("click", calculatorBMI);
+const bmi = (weight / (convert*convert)).toFixed(2);
+console.log(bmi);
 
 if(0<=bmi && bmi<18.5){
     alert('Underweight');
-     }else if(18.5<=bmi && bmi<=24.9){
+}else if(18.5<=bmi && bmi<=24.9){
     alert('Healthy');
-     }else if(25<=bmi && bmi<=29.9){
+}else if(25<=bmi && bmi<=29.9){
     alert('Overweight');
-     }else if(30<=bmi && bmi<=34.9){
+}else if(30<=bmi && bmi<=34.9){
     alert('Obese');
-     }else if(35<=bmi){
+}else if(35<=bmi){
     alert('Extremely obese');
-     }else
+}else
     alert('Please enter the valid value');
 }
